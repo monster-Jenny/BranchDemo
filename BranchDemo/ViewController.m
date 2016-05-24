5474d488e44a7a41607abeb81fc150da3a36b579
Monster Wang <rr.wang@zuolin.com>
2016-05-24 14:48:14 +0800

    主分支

--- BranchDemo/ViewController.m
+++ BranchDemo/ViewController.m
@@ -17,6 +17,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSString * str = @"这是主分支";
+    NSLog(@"%@",str);
+    
 }
 
 - (void)didReceiveMemoryWarning {
